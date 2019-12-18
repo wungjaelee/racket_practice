9d14ae221f3f925a31d09ec5cdd8ea9b51511b29
Wung Jae Lee <wunglee2021@u.northwestern.edu>
2019-12-17 23:47:01 -0600

    quick introduction to racket

---
+++ quick.rkt
@@ -0,0 +1,83 @@
+#lang slideshow
+(define c (circle 10))
+(define r (rectangle 10 20))
+(define (square n)
+  (filled-rectangle n n))
+
+(define (four p)
+  (define two-p (hc-append p p))
+  (vc-append two-p two-p))
+
+(four (circle 10))
+
+(define (checker p1 p2)
+  (let ([p12 (hc-append p1 p2)]
+        [p21 (hc-append p2 p1)])
+    (vc-append p12 p21)))
+
+(checker (colorize (square 10) "red")
+         (colorize (square 10) "black"))
+
+(define (checkerboard p)
+  (let* ([rp (colorize p "red")]
+         [bp (colorize p "black")]
+         [c (checker rp bp)]
+         [c4 (four c)])
+    (four c4)))
+
+(checkerboard (square 10))
+
+(define (series mk)
+  (hc-append 4 (mk 5) (mk 10) (mk 20)))
+
+(series circle)
+(series square)
+
+(series (lambda (size) (checkerboard (square size))))
+
+(define (rgb-series mk)
+  (vc-append
+   (series (lambda (sz) (colorize (mk sz) "red")))
+   (series (lambda (sz) (colorize (mk sz) "green")))
+   (series (lambda (sz) (colorize (mk sz) "blue")))))
+
+(define (rgb-maker mk)
+  (lambda (sz)
+    (vc-append (colorize (mk sz) "red")
+               (colorize (mk sz) "green")
+               (colorize (mk sz) "blue"))))
+
+(define (rainbow p)
+  (map (lambda (color)
+         (colorize p color))
+       (list "red" "orange" "yellow" "green" "blue" "purple")))
+
+(require pict/flash)
+
+; Macros
+(require slideshow/code)
+
+(define-syntax pict+code
+  (syntax-rules ()
+    [(pict+code expr)
+     (hc-append 10
+                expr
+                (code expr))]))
+
+; 11 Objects
+(require racket/class
+         racket/gui/base)
+
+(define f (new frame% [label "My Art"]
+                      [width 300]
+                      [height 300]
+                      [alignment '(center center)]))
+
+;(send f show #t)
+
+(define (add-drawing p)
+  (let ([drawer (make-pict-drawer p)])
+    (new canvas% [parent f]
+                 [style '(border)]
+                 [paint-callback (lambda (self dc)
+                                   (drawer dc 0 0))])))
